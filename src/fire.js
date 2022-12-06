b9fa2fb70c5d9019bd2645fb5e2483df7317cda5
Alex Foster <afoster1@iastate.edu>
2022-12-05 21:51:48 -0600

    testing noise

--- src/fire.js
+++ src/fire.js
@@ -1,17 +1,42 @@
-import "open-simplex-noise";
+
 // Basic example of loading an image as a texture and mapping it onto a
 // square. Edit the coordinates of the square or edit the texture coordinates
-// to experiment.  Image filename is given directly below.  Use key
-// controls to experiment with magnification filter or wrapping behavior.
+// to experiment.  Image filename is given directly below.  Use the "1" and
+// "2" keys to switch from NEAREST to LINEAR magnification filter.
 //
 // For security reasons the browser restricts access to the local filesystem.
 // To run the example, open a command shell in any directory above your examples
 // and your teal book utilities, and run python -m SimpleHttpServer 2222
 // or python3 -m http.server
 // Then point your browser to http://localhost:2222 and navigate to the
-// example you want to run.
+// example you want to run.  For alternatives see
+// https://github.com/mrdoob/three.js/wiki/How-to-run-things-locally
+//
 
-
+// <script id="vertexShader" type="x-shader/x-vertex">
+// attribute vec4 a_Position;
+// attribute vec2 a_TexCoord;
+// varying vec2 fTexCoord;
+// void main()
+// {
+//   // pass through so the value gets interpolated
+//   fTexCoord = a_TexCoord;
+//   gl_Position = a_Position;
+// }
+// </script>
+//
+// <script id="fragmentShader" type="x-shader/x-fragment">
+// precision mediump float;
+// uniform sampler2D sampler;
+// varying vec2 fTexCoord;
+// void main()
+// {
+//   // sample from the texture at the interpolated texture coordinate,
+//   // and use the value directly as the surface color
+//   vec4 color = texture2D(sampler, fTexCoord);
+//   gl_FragColor = color;
+// }
+// </script>
 
 // vertex shader
 const vshaderSource = `
@@ -29,6 +54,7 @@ void main()
 // fragment shader
 const fshaderSource = `
 precision mediump float;
+uniform int state;
 uniform sampler2D sampler;
 varying vec2 fTexCoord;
 void main()
@@ -36,17 +62,96 @@ void main()
   // sample from the texture at the interpolated texture coordinate,
   // and use the value directly as the surface color
   vec4 color = texture2D(sampler, fTexCoord);
-  gl_FragColor = color;
+
+#if 1
+  // use a greyscale value in [0, 1], exaggerate contrast
+  float c = 0.0;
+  if (state == 0)
+    c = color.r; // * 2.0;
+  else if (state == 1)
+    c = color.g; // * 4.0;
+  else if (state == 2)
+    c = color.b; // * 8.0;
+  else if (state == 3)
+    c = color.a; // * 16.0;
+
+  gl_FragColor = vec4(c, c, c, 1.0);
+
+#endif
+
+#if 0
+
+  // turbulence (absolute value of noise)
+
+  float fraction = color.r;  // 3.0;
+  fraction = abs((fraction - 0.5) * 3.0); // recenter and scale up
+    vec4 red = vec4(1.0, 0.0, 0.0, 1.0);
+    vec4 yellow = vec4(1.0, 1.0, 0.0, 1.0);
+    vec4 c = fraction * red + (1.0 - fraction) * yellow;
+    c.a = 1.0;
+    gl_FragColor = c;
+
+#endif
+
 }
 `;
 
 
-//var imageFilename = "../images/check64.png";
-var imageFilename = "../images/check64border.png";
-//var imageFilename = "../images/uv_grid.jpg";
-//var imageFilename = "../images/clover_really_small.jpg";
-//var imageFilename = "../images/brick.png";
-//var imageFilename = "../images/steve.png";
+// edit to configure the texture (which is created around line 410 using
+// the createNoiseTexture function below.)
+var size = 256;
+var frequency = 4;
+var octaves = 2;
+
+var noiseMaker = new ClassicalNoise();
+
+// Creates noise texture at four frequencies starting at the given
+// frequency, stored in the red, green, blue, and alpha channels
+// of the texture.  The 'octaves'
+function createNoiseTexture(size, frequency, octaves, time)
+{
+
+  var canvas = document.createElement('canvas');
+
+  canvas.height=size;
+  canvas.width=size;
+
+  //
+  // create an image programmatically by drawing to an offscreen html canvas
+  //
+  var context = canvas.getContext("2d");
+  var imageData=context.createImageData(size, size);
+  // The property 'data' will contain an array of int8
+  var data=imageData.data;
+
+  let delta = (1.0 / size);
+  for (let i = 0; i < size; ++i)
+  {
+    let x = i * delta;
+    for (let j = 0; j < size; ++j)
+    {
+      let y = j * delta;
+      let base = i * size + j;
+      let nn = 0.0;
+      let f = frequency;
+      octaves = 4;
+      for (let k = 0; k < octaves; ++k)
+      {
+        nn += noiseMaker.noise(x * f, y * f + time, 0) / f;
+
+        // values appear to be about +/- .27, so scale appropriately
+        // to store as a color value in [0, 255]
+        let nnn = (nn + 0.5) * 256;
+        data[base * 4 + k] = nnn;
+        f *= 2;
+      }
+
+    } // j loop
+  } // i loop
+
+  context.putImageData(imageData, 0, 0); // at coords 0,0
+  return canvas;
+}
 
 
 // Raw data for some point positions - this will be a square, consisting
@@ -55,25 +160,14 @@ var imageFilename = "../images/check64border.png";
 var numPoints = 6;
 
  var vertices = new Float32Array([
--0.5, -0.5,
+0.0, -0.5,
 0.5, -0.5,
 0.5, 0.5,
--0.5, -0.5,
+0.0, -0.5,
 0.5, 0.5,
--0.5, 0.5
+0.0, 0.5
 ]
 );
-
-// alternatively, try vertices for a trapezoid instead of square
-//  var vertices = new Float32Array([
-// -0.5, -0.5,
-// 0.5, -0.5,
-// 0.25, 0.5,
-// -0.5, -0.5,
-// 0.25, 0.5,
-// -0.25, 0.5
-//  ]
-//  );
 
 // most straightforward way to choose texture coordinates
 var texCoords = new Float32Array([
@@ -99,8 +193,9 @@ var shader;
 
 // handle to the texture object on the GPU
 var textureHandle;
+var state = 0;
 
-
+var imageFilename = "../images/fireimg.png"
 
 //translate keypress events to strings
 //from http://javascript.info/tutorial/keyboard-events
@@ -123,38 +218,30 @@ var ch = getChar(event);
 switch(ch)
 {
 
-
-case 'm':
-  gl.bindTexture(gl.TEXTURE_2D, textureHandle);
-  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
+  case '0':
+    state = 0;
+    break;
+case '1':
+  state = 1;
   break;
-case 'M':
-  gl.bindTexture(gl.TEXTURE_2D, textureHandle);
-  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+case '2':
+  state = 2;
   break;
-case 'n':
-  gl.bindTexture(gl.TEXTURE_2D, textureHandle);
-  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+case '3':
+  state = 3;
   break;
-case 'N':
-  gl.bindTexture(gl.TEXTURE_2D, textureHandle);
-  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
-  break;
-case 's':
-  gl.bindTexture(gl.TEXTURE_2D, textureHandle);
-  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
-  break;
-case 'S':
-  gl.bindTexture(gl.TEXTURE_2D, textureHandle);
-  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.REPEAT);
-  break;
-case 't':
-  gl.bindTexture(gl.TEXTURE_2D, textureHandle);
-  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
-  break;
-case 'T':
-  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.REPEAT);
-  break;
+  case '4':
+    state = 4;
+    break;
+    case '5':
+      state = 5;
+      break;
+      case '6':
+        state = 6;
+        break;
+        case '7':
+          state = 7;
+          break;
 
 
   default:
@@ -213,7 +300,7 @@ function draw()
   gl.bindBuffer(gl.ARRAY_BUFFER, null);
 
   // need to choose a texture unit, then bind the texture to TEXTURE_2D for that unit
-  var textureUnit = 0;
+  var textureUnit = 3;
   gl.activeTexture(gl.TEXTURE0 + textureUnit);
   gl.bindTexture(gl.TEXTURE_2D, textureHandle);
 
@@ -227,6 +314,10 @@ function draw()
   // sampler value in shader is set to index for texture unit
   gl.uniform1i(loc, textureUnit);
 
+  loc = gl.getUniformLocation(shader, "state");
+  gl.uniform1i(loc, state);
+
+
   // draw, specifying the type of primitive to assemble from the vertices
   gl.drawArrays(gl.TRIANGLES, 0, numPoints);
   //gl.drawArrays(gl.LINES, 0, numPoints);
@@ -238,59 +329,37 @@ function draw()
 
 }
 
-// entry point when page is loaded.  Wait for image to load before proceeding
-// function main() {
-//   var image = new Image();
-//   image.onload = function() { startForReal(image); };
-//
-//   // starts loading the image asynchronously
-//   image.src = imageFilename;
-// }
-
 
 async function main(image) {
 
-  var image = await loadImagePromise(imageFilename);
+  var fire = await loadImagePromise(imageFilename);
+
+  // basically this function does setup that "should" only have to be done once,
+  // while draw() does things that have to be repeated each time the canvas is
+  // redrawn
 
   // key handler
   window.onkeypress = handleKeyPress;
 
-  // get graphics context using its id
+  // get graphics context
   gl = getGraphicsContext("theCanvas");
-
-  // query the GPU for the number of available texture units
-  // console.log("Texture units: " + gl.getParameter(gl.MAX_COMBINED_TEXTURE_IMAGE_UNITS));
-  // var ext = gl.getExtension("EXT_texture_filter_anisotropic");
-  // if (ext)
-  // {
-  //   var max_anisotropy = gl.getParameter(ext.MAX_TEXTURE_MAX_ANISOTROPY_EXT);
-  //   console.log("Anisotropy: " + max_anisotropy)
-  // }
-
 
   // load and compile the shader pair
   shader = createShaderProgram(gl, vshaderSource, fshaderSource);
 
   // load the vertex data into GPU memory
   vertexbuffer = createAndLoadBuffer(vertices);
-
-  // load the texture coordinates into GPU memory
   texCoordBuffer = createAndLoadBuffer(texCoords);
 
   // specify a fill color for clearing the framebuffer
   gl.clearColor(0.0, 0.8, 0.8, 1.0);
 
+  // create the noise texture
+  image = createNoiseTexture(size, frequency, octaves);
+
   // ask the GPU to create a texture object
   textureHandle = createAndLoadTexture(image);
-
-  // can set texture parameters, see key controls
-  //gl.bindTexture(gl.TEXTURE_2D, textureHandle);
-  //gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
-
-  // can also change the wrapping behavior, default is gl.REPEAT
-  //gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
-  //gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
-
+  fireHandle = createAndLoadTexture(fire);
 
   // define an animation loop
   var animate = function() {
