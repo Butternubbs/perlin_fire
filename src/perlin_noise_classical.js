1e5555160ebb7b1f8c43ee3b413d053d1894007b
Alex Foster <afoster1@iastate.edu>
2022-12-07 12:48:48 -0600

    added a 4d perlin noise function

--- src/perlin_noise_classical.js
+++ src/perlin_noise_classical.js
@@ -31,8 +31,16 @@
  
      if (r == undefined) r = Math;
    this.grad3 = [[1,1,0],[-1,1,0],[1,-1,0],[-1,-1,0],
-                                  [1,0,1],[-1,0,1],[1,0,-1],[-1,0,-1],
-                                  [0,1,1],[0,-1,1],[0,1,-1],[0,-1,-1]];
+                 [1,0,1],[-1,0,1],[1,0,-1],[-1,0,-1],
+                 [0,1,1],[0,-1,1],[0,1,-1],[0,-1,-1]];
+   this.grad4 = [[0,1,1,1], [0,1,1,-1], [0,1,-1,1], [0,1,-1,-1],
+                 [0,-1,1,1], [0,-1,1,-1], [0,-1,-1,1], [0,-1,-1,-1],
+                 [1,0,1,1], [1,0,1,-1], [1,0,-1,1], [1,0,-1,-1],
+                 [-1,0,1,1], [-1,0,1,-1], [-1,0,-1,1], [-1,0,-1,-1],
+                 [1,1,0,1], [1,1,0,-1], [1,-1,0,1], [1,-1,0,-1],
+                 [-1,1,0,1], [-1,1,0,-1], [-1,-1,0,1], [-1,-1,0,-1],
+                 [1,1,1,0], [1,1,-1,0], [1,-1,1,0], [1,-1,-1,0],
+                 [-1,1,1,0], [-1,1,-1,0], [-1,-1,1,0], [-1,-1,-1,0]];
    this.p = [];
    for (var i=0; i<256; i++) {
        this.p[i] = Math.floor(r.random()*256);
@@ -45,9 +53,13 @@
    }
  };
  
- ClassicalNoise.prototype.dot = function(g, x, y, z) {
+ ClassicalNoise.prototype.dot3 = function(g, x, y, z) {
      return g[0]*x + g[1]*y + g[2]*z;
  };
+
+ ClassicalNoise.prototype.dot4 = function(g, x, y, z, w) {
+  return g[0]*x + g[1]*y + g[2]*z + g[3]*w;
+};
  
  ClassicalNoise.prototype.mix = function(a, b, t) {
      return (1.0-t)*a + t*b;
@@ -58,7 +70,7 @@
  };
  
    // Classic Perlin noise, 3D version
- ClassicalNoise.prototype.noise = function(x, y, z) {
+ ClassicalNoise.prototype.noise3 = function(x, y, z) {
    // Find unit grid cell containing point
    var X = Math.floor(x);
    var Y = Math.floor(y);
@@ -94,14 +106,14 @@
    // g110 = grad3[gi110];
    // g111 = grad3[gi111];
    // Calculate noise contributions from each of the eight corners
-   var n000= this.dot(this.grad3[gi000], x, y, z);
-   var n100= this.dot(this.grad3[gi100], x-1, y, z);
-   var n010= this.dot(this.grad3[gi010], x, y-1, z);
-   var n110= this.dot(this.grad3[gi110], x-1, y-1, z);
-   var n001= this.dot(this.grad3[gi001], x, y, z-1);
-   var n101= this.dot(this.grad3[gi101], x-1, y, z-1);
-   var n011= this.dot(this.grad3[gi011], x, y-1, z-1);
-   var n111= this.dot(this.grad3[gi111], x-1, y-1, z-1);
+   var n000= this.dot3(this.grad3[gi000], x, y, z);
+   var n100= this.dot3(this.grad3[gi100], x-1, y, z);
+   var n010= this.dot3(this.grad3[gi010], x, y-1, z);
+   var n110= this.dot3(this.grad3[gi110], x-1, y-1, z);
+   var n001= this.dot3(this.grad3[gi001], x, y, z-1);
+   var n101= this.dot3(this.grad3[gi101], x-1, y, z-1);
+   var n011= this.dot3(this.grad3[gi011], x, y-1, z-1);
+   var n111= this.dot3(this.grad3[gi111], x-1, y-1, z-1);
    // Compute the fade curve value for each of x, y, z
    var u = this.fade(x);
    var v = this.fade(y);
@@ -119,4 +131,95 @@
  
    return nxyz;
  };
+
+ ClassicalNoise.prototype.noise4 = function(x, y, z, w) {
+  // Find unit grid cell containing point
+  var X = Math.floor(x);
+  var Y = Math.floor(y);
+  var Z = Math.floor(z);
+  var W = Math.floor(w);
+
+  // Get relative xyz coordinates of point within that cell
+  x = x - X;
+  y = y - Y;
+  z = z - Z;
+  w = w - W;
+
+  // Wrap the integer cells at 255 (smaller integer period can be introduced here)
+  X = X & 255;
+  Y = Y & 255;
+  Z = Z & 255;
+  W = W & 255;
+
+  // Calculate a set of eight hashed gradient indices
+  var gi0000 = this.perm[X+this.perm[Y+this.perm[Z+this.perm[W]]]] % 12;
+  var gi0010 = this.perm[X+this.perm[Y+this.perm[Z+1+this.perm[W]]]] % 12;
+  var gi0100 = this.perm[X+this.perm[Y+1+this.perm[Z+this.perm[W]]]] % 12;
+  var gi0110 = this.perm[X+this.perm[Y+1+this.perm[Z+1+this.perm[W]]]] % 12;
+  var gi1000 = this.perm[X+1+this.perm[Y+this.perm[Z+this.perm[W]]]] % 12;
+  var gi1010 = this.perm[X+1+this.perm[Y+this.perm[Z+1+this.perm[W]]]] % 12;
+  var gi1100 = this.perm[X+1+this.perm[Y+1+this.perm[Z+this.perm[W]]]] % 12;
+  var gi1110 = this.perm[X+1+this.perm[Y+1+this.perm[Z+1+this.perm[W]]]] % 12;
+  var gi0001 = this.perm[X+this.perm[Y+this.perm[Z+this.perm[W+1]]]] % 12;
+  var gi0011 = this.perm[X+this.perm[Y+this.perm[Z+1+this.perm[W+1]]]] % 12;
+  var gi0101 = this.perm[X+this.perm[Y+1+this.perm[Z+this.perm[W+1]]]] % 12;
+  var gi0111 = this.perm[X+this.perm[Y+1+this.perm[Z+1+this.perm[W+1]]]] % 12;
+  var gi1001 = this.perm[X+1+this.perm[Y+this.perm[Z+this.perm[W+1]]]] % 12;
+  var gi1011 = this.perm[X+1+this.perm[Y+this.perm[Z+1+this.perm[W+1]]]] % 12;
+  var gi1101 = this.perm[X+1+this.perm[Y+1+this.perm[Z+this.perm[W+1]]]] % 12;
+  var gi1111 = this.perm[X+1+this.perm[Y+1+this.perm[Z+1+this.perm[W+1]]]] % 12;
+
+  // The gradients of each corner are now:
+  // g000 = grad3[gi000];
+  // g001 = grad3[gi001];
+  // g010 = grad3[gi010];
+  // g011 = grad3[gi011];
+  // g100 = grad3[gi100];
+  // g101 = grad3[gi101];
+  // g110 = grad3[gi110];
+  // g111 = grad3[gi111];
+  // Calculate noise contributions from each of the eight corners
+  var n0000= this.dot3(this.grad4[gi0000], x, y, z, w);
+  var n1000= this.dot3(this.grad4[gi1000], x-1, y, z, w);
+  var n0100= this.dot3(this.grad4[gi0100], x, y-1, z, w);
+  var n1100= this.dot3(this.grad4[gi1100], x-1, y-1, z, w);
+  var n0010= this.dot3(this.grad4[gi0010], x, y, z-1, w);
+  var n1010= this.dot3(this.grad4[gi1010], x-1, y, z-1, w);
+  var n0110= this.dot3(this.grad4[gi0110], x, y-1, z-1, w);
+  var n1110= this.dot3(this.grad4[gi1110], x-1, y-1, z-1, w);
+  var n0001= this.dot3(this.grad4[gi0001], x, y, z, w-1);
+  var n1001= this.dot3(this.grad4[gi1001], x-1, y, z, w-1);
+  var n0101= this.dot3(this.grad4[gi0101], x, y-1, z, w-1);
+  var n1101= this.dot3(this.grad4[gi1101], x-1, y-1, z, w-1);
+  var n0011= this.dot3(this.grad4[gi0011], x, y, z-1, w-1);
+  var n1011= this.dot3(this.grad4[gi1011], x-1, y, z-1, w-1);
+  var n0111= this.dot3(this.grad4[gi0111], x, y-1, z-1, w-1);
+  var n1111= this.dot3(this.grad4[gi1111], x-1, y-1, z-1, w-1);
+  // Compute the fade curve value for each of x, y, z
+  var s = this.fade(x);
+  var t = this.fade(y);
+  var u = this.fade(z);
+  var v = this.fade(w);
+   // Interpolate along x the contributions from each of the corners
+  var nx000 = this.mix(n0000, n1000, s);
+  var nx001 = this.mix(n0001, n1001, s);
+  var nx010 = this.mix(n0010, n1010, s);
+  var nx011 = this.mix(n0011, n1011, s);
+  var nx100 = this.mix(n0100, n1100, s);
+  var nx101 = this.mix(n0101, n1101, s);
+  var nx110 = this.mix(n0110, n1110, s);
+  var nx111 = this.mix(n0111, n1111, s);
+  // Interpolate the four results along y
+  var nxy00 = this.mix(nx000, nx100, t);
+  var nxy01 = this.mix(nx001, nx101, t);
+  var nxy10 = this.mix(nx010, nx110, t);
+  var nxy11 = this.mix(nx011, nx111, t);
+  // Interpolate the two last results along z
+  var nxyz0 = this.mix(nxy00, nxy10, u);
+  var nxyz1 = this.mix(nxy01, nxy11, u);
+
+  var nxyzw = this.mix(nxyz0, nxyz1, v);
+
+  return nxyzw;
+};
  
